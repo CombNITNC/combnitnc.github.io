d72df763d074e724965f5dfa73bcf2ff3fec2358
MikuroXina <ryosukadnak@gmail.com>
2022-12-19 20:05:11 +0900

    Simplify pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,5 @@
-import Link from 'next/link'
-
 export default function Home() {
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
+    <div>Hello World!</div>
   )
 }
